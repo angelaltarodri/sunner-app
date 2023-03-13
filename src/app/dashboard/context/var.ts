256ab09f7cc94fc01e134cff601074dbd3bc730e
Angel <angelaltarodri@gmail.com>
2023-03-13 17:11:28 -0500

    font poppins + plantInfo

---
+++ src/app/dashboard/context/var.ts
@@ -0,0 +1,6 @@
+
+
+
+
+export const ahorroCO2: number = 0.0004639
+export const ahorroEnS: number = 0.7;
--- src/app/dashboard/pages/main/main.component.ts
+++ src/app/dashboard/pages/main/main.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlantInfo } from 'src/app/auth/interfaces/plantInfo.interface';
+import { ahorroCO2, ahorroEnS } from '../../context/var';
+import { PlantServiceService } from '../../services/plant-service.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  energiaTotalConsumida:number = 0;
+  ahorroCO2: number = ahorroCO2;
+  ahorroEnS: number = ahorroEnS;
+
+  constructor(
+    private plantService: PlantServiceService,
+  ) {}
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    this.plantService.getPlantInfo(token!)
+      .subscribe( res => this.energiaTotalConsumida = Number(res.totalEnergy));
+  }
+
+  showPlantInfo () {
+    console.log(this.energiaTotalConsumida)
+  }
+
+  get ahorroMonetario() {
+    return this.energiaTotalConsumida*this.ahorroEnS;
+  }
+  get ahorroEmisiones() {
+    return this.energiaTotalConsumida*this.ahorroCO2;
+  }
 
 }
