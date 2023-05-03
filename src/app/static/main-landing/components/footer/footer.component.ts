1a3e34896998daae2767bad8611f5c8f511be959
Angel Altamirano <angelaltarodri@gmail.com>
2023-05-03 00:32:02 -0500

    social media implemented on landing

--- src/app/static/main-landing/components/footer/footer.component.ts
+++ src/app/static/main-landing/components/footer/footer.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+type RRSSKeys = 'facebook' | 'instagram' | 'linkedin' | 'youtube';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  RRSS: Record<RRSSKeys, string> = {
+    facebook: 'https://www.facebook.com/sunnerperu',
+    instagram: 'https://www.instagram.com/sunnerperu/',
+    linkedin: 'https://www.linkedin.com/company/sunnerperu/',
+    youtube: '',
+  };
 
+  openRRSS(rrss: RRSSKeys) {
+    window.open(this.RRSS[rrss]);
+  }
 }
